dc8e0fea7c71ed27414e8d76992118b085083ae0
Maaz Ahmed <elfilosof@protonmail.com>
2024-11-10 13:45:59 -0800

    fixed typos in item and receipt models

--- models.go
+++ models.go
@@ -2,12 +2,12 @@ package main
 
 type Item struct {
 	ShortDescription string `json:"shortDescription"`
-	Price            string `json:"Price"`
+	Price            string `json:"price"`
 }
 
 type Receipt struct {
 	Retailer     string `json:"retailer"`
-	PurchaseDate string `json:"purchaseDaete"`
+	PurchaseDate string `json:"purchaseDate"`
 	PurchaseTime string `json:"purchaseTime"`
 	Items        []Item `json:"items"`
 	Total        string `json:"total"`
